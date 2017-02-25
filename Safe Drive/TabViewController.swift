dd5ca6b3e4cef8566cce88acb620a1a26251a347
packetloss404 <ianwalmsley@ianlan.net>
2017-02-25 01:23:48 -0600

    tabview names, app icon

---
+++ Safe Drive/TabViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TabViewController.swift
+//  Safe Drive
+//
+//  Created by Ian Walmsley on 2/25/17.
+//  Copyright © 2017 JISS-ISU. All rights reserved.
+//
+
+import UIKit
+
+class TabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tabBar.items?[0].title = "Safe Ride";
+        tabBar.items?[1].title = "About";
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
